310df72aee35b98e37a20c20046024f25ac5d548
SamStudy9527 <1210168670@qq.com>
2020-02-18 09:58:41 +0800

    tag：新建项目。导入基础文件夹

---
+++ Assets/Scptris/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+   
+
+    private void Awake()
+    {
+        StartDeal();
+    }
+
+    private void StartDeal()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scptris/Role.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Role : MonoBehaviour
+{
+  
+    /// <summary> 角色身份 </summary>
+    Dictionary<int, Identity> RoleIdentity = new Dictionary<int, Identity>();
+    /// <summary> 角色阵营 </summary>
+    Dictionary<int, Camp> RoleCamp = new Dictionary<int, Camp>();
+    /// <summary> 角色阵营剩余人数 </summary>
+    private int GodCamp, ManCamp, WolfMan;
+
+
+    /// <summary> 身份枚举 </summary>
+    private enum Identity
+    {
+        Bad,
+        Good
+    }
+
+    /// <summary> 阵营枚举 </summary>
+    private enum Camp
+    {
+        WolfMan,
+        Man,
+        God,
+    }
+
+    void Init()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+
+        }
+    }
+
+}
